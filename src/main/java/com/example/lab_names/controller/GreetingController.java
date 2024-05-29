00742cd2eb6cacbdb9929421a52d5f0a58de4ddc
Rabin Kandel <kandelrabin@live.com>
2024-05-29 14:53:18 +0100

    Complete MVP part 3 and 4

--- src/main/java/com/example/lab_names/controller/GreetingController.java
+++ src/main/java/com/example/lab_names/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.lab_names.controller;
 
+import com.example.lab_names.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,9 @@ public class GreetingController {
 
     @GetMapping
     public String greet(@RequestParam(defaultValue = "") String name){
-        return String.format("Good afternoon %s!",name);
+        Greeting greeting = new Greeting(name, "afternoon");
+        return String.format("Good %s %s!",greeting.getTimeOfDay(), greeting.getName());
     }
+
+
 }
--- src/main/java/com/example/lab_names/models/Greeting.java
+++ src/main/java/com/example/lab_names/models/Greeting.java
@@ -1,4 +1,33 @@
 package com.example.lab_names.models;
 
 public class Greeting {
+
+    private String name;
+    private String timeOfDay;
+
+    public Greeting(String name, String timeOfDay){
+        this.name = name;
+        this.timeOfDay = timeOfDay;
+    }
+
+    public Greeting(){
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeOfDay() {
+        return this.timeOfDay;
+    }
+
+    public void setTimeOfDay(String timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+
 }
